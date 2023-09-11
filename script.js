50074bcf79927485600b496763484af83d962780
Ikenna Richard <oguejioforrichard@gmail.com>
2023-09-10 18:41:14 -1100

    Add form validation

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form');
+const password = form.querySelector('input[id=password]');
+const confirmPassword = form.querySelector('input[id=confirm-password]');
+const error = form.querySelector('.error');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if (password.value !== confirmPassword.value) {
+    error.classList.add('error-show');
+    password.style.outline = '1px solid red';
+    setTimeout(() => {
+      error.classList.remove('error-show');
+      password.style.outline = '';
+    }, 2000);
+  };
+});
